9691b256a25207ab3251a4e1e4ffb2edf739c93b
AccroDev <96702637+AccroDev@users.noreply.github.com>
2025-10-03 21:29:54 +0200

    check tailwind

--- index.php
+++ index.php
@@ -1,66 +1,12 @@
-<?php 
-$_clesh ="je suis clesh j'apprends le php";
-
-echo($_clesh);
-
-$max = 20;
-$min =20;
-
-if ($max == $min){
-    echo("mean est egale a maix");
-}
-$users = ["nom" => "clesh","prenom" => "matondo"];
-echo($users["prenom"]);
- 
-$paye = ["RDC","RC","GABON"];
-echo($paye[2]);
-
-echo($max-$min);
-
-$nom ="clesh";
-$prenom ="matondo";
-echo( "<br>");
-echo($nom.$prenom );
-echo( "<br>");
-echo($nom . " ". $prenom );
-echo( "<br>");
-foreach ($users as $key => $value) {
-    echo($key. ":" . $value );
-    echo( "<br>");
-}
-
-foreach ($paye as $key => $value) {
-    echo($key. ":" . $value );
-    echo( "<br>");
-}
-foreach ($paye as $value) {
-    echo($value );
-    echo( "<br>");
-}
-  echo( "<br>");  
-
-    echo( "<br>");
-for ($i=0; $i <3 ; $i++) { 
-    echo($paye[$i]);
-    echo( "<br>");
-}
-
-function affichage($text,$nbr){
-    echo("$text");
-    for ($i=0; $i < $nbr; $i++) { 
-          echo("<br>");
-    }
-   
-}
-affichage("je suis clesh",4);
-
-affichage("hello word",2);
-affichage("richard",3);
-
-function concact($nom,$prenom){
-    return($nom . " " . $prenom);
-}
-$fullname = concact("clesh","ngambou");
-affichage($fullname,4);
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="p-5 " >Hello word</div>
+</body>
+</html>
