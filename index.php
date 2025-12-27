1f925d0bc3eabe6d432d1a963422f9cc334edf00
Clesh <Cleshcleshng02@gmail.com>
2025-12-27 18:38:33 +0100

    hello

--- index.php
+++ index.php
@@ -2,11 +2,52 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Connexion et inscription </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='bootstrap.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='all.min.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styles.css'>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div class="p-5 " >Hello word</div>
+<body class="bg-light">
+    <div class="container ">
+        <div class="row mt-5">
+            <div class="col-lg-4 bg-white m-auto rounded-top">
+                <h2 class="text-center"> Connexion</h2>
+                <p class="text-center text-muted lead"> Se connecter à Cleshng </p>
+
+                <form action="">
+                    <div class="input-group  mb-3">
+                        <span class="input-group-text">
+                            <i class="fa fa-envelope">
+                            </i> 
+                        </span>
+                        <input type="text" class="form-control" placeholder="Adresse e-mail  ">
+                    </div>
+                
+                    <div class="input-group  mb-3">
+                        <span class="input-group-text">
+                            <i class="fa fa-lock">
+                            </i> 
+                        </span>
+                        <input type="text" class="form-control" placeholder="Mot de passe ">
+                    </div>
+                    <div class="d-grid">
+                        <button type="button" class="btn btn-success">Se connecter</button>
+                       
+                        <p class="text-center">
+                              vous n'avez pas de compte ?<a href="index.html"> Inscription </a>
+                        </p>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    
 </body>
+</html> 
+<script src='js/bootstrap.js'></script>
 </html>
